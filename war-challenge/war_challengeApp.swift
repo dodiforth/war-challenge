eaf35ee863067e4500007f146c1e6880255ed30a
dowoniscool <dowoniscool@yahoo.com>
2020-12-29 21:43:32 +0100

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by dowoniscool on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
