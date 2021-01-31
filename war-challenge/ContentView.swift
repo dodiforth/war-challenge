6514b114661d0a60d8ea876e280fe8a6682ad95b
Dowon Kim <dowoniscool@yahoo.com>
2021-02-01 01:31:18 +0900

    Finish the war card game Previously learned about:
    1. How to reference properties in my view code(Using properties in viewcode.)
    2. The state property wapper.
    3. If Statements.

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   @State private var playerCard = "card5"
+   @State private var cpuCard = "card9"
+   @State private var playerScore = 0
+   @State private var cpuScore = 0
+    
     var body: some View {
         
         ZStack {
@@ -23,16 +29,34 @@ struct ContentView: View {
                 
                 HStack{
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
                     Spacer()
-                    Image("card4")
+                    Image(cpuCard)
                     Spacer()
                 }
                 
                 Spacer()
                 
                 Button(action: {
-                    print("This is DEAL button")
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    // Update the scores
+                    if playerRand > cpuRand {
+                        return playerScore += 1
+                    } else if cpuRand > playerRand {
+                        return cpuScore += 1
+                    } else {
+                        print("TIE!")
+                    }
+                    
+                    //playerScore += 1
+                    //cpuScore += 1
+                    
                 }, label: {
                     Image("dealbutton")
                 })
@@ -48,7 +72,7 @@ struct ContentView: View {
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
                         
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
@@ -58,7 +82,7 @@ struct ContentView: View {
                             .font(.headline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
